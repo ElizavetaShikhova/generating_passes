2a2da19292299cd1822010194c72eac50925282d
meanlessnessener <mr.sansanich05@mail.ru>
2022-06-27 17:23:39 +0500

    i've learnt more about exceptions and have fixed cringe exception handling. also there's added some litlle changes that don't matter but make code more fine

--- genpdf.py
+++ genpdf.py
@@ -19,7 +19,7 @@ class GenPdf:
         self.pdf.set_text_color(r, g, b)
         self.pdf.set_font_size(size)
 
-    # данный метод генерирования шаблона пригождается для пропуска одному человеку и гостю/посетителю
+    # данный метод генерирования шаблона пригождается для создания пропуска одному человеку и гостю/посетителю
     def __create_template(self):
         self.pdf.image('src/u.png', x=5, y=3, w=9.4, h=9, type='PNG')  # рисуем букву У
 
@@ -33,8 +33,7 @@ class GenPdf:
         self.pdf.text(20, 10, 'СПЕЦИАЛИЗИРОВАННЫЙ УЧЕБНО-НАУЧНЫЙ ЦЕНТР')
 
     # метод для создания одной странички pdf одному человеку
-    # если все плохо, то возвращает текст ошибки (а если все хорошо, то молчит)
-    def create_person(self, person: Person) -> str:
+    def create_person(self, person: Person):
         self.pdf.add_page()  # добавляем страничку
 
         self.__create_template()  # рисуем шаблончик
@@ -69,35 +68,27 @@ class GenPdf:
 
         try:
             self.pdf.image(person.photo, 5, 14, 24, 32, type='JPG')  # если все круто, то маленький художник, который сидит в FPDF.pdf.image, рисует главного героя этого пропуска
-        except Exception as e:  # но всегда что-то может пойти не так, например, фото не доступно
-            return f'Фотография для {person.name + " " + person.surname} не найдена'
-            # волноваться не стоит - текст ошибки всегда один и тот же. он всегда говорит, что файлик не нашелся
+        except RuntimeError:  # но всегда что-то может пойти не так, например, фото недоступно
+            raise Exception(f'Фотография для {person.name + " " + person.surname} не найдена')
 
         self.pdf.set_font('PFM', 'bold')  # пока вместо начального шрифта властвовал "нормальный", тот успел навестить бабушку в деревне и от этого потолстеть (ох уж эти бабушки, всегда тебя накормят, даже если ты только что сотню пирожков съел)
         self.set_color_n_size(0, 128, 0, 7)
         event_name = 'МЕРОПРИЯТИЯ' if person.status == Status.participant else 'ОБУЧЕНИЯ'  # название "мероприятия" в родительном падеже идет в переменную
         self.pdf.text(20, 51, f'ПО ОКОНЧАНИИ {event_name} ПОДЛЕЖИТ ВОЗВРАТУ')
 
-        return ''  # а если все круто, то и текст ошибки возвращать не надо, какой же прекрасной иногда бывает жизнь
-
     # метод для генерации пропусков для всех, кто есть в списке
-    # если все плохо, то возвращает текст ошибки (а если все хорошо, то молчит)
-    def create_group(self, people: List[Person]) -> str:
+    def create_group(self, people: List[Person]):
         for person in people:
-            alles_kaputt = self.create_person(person)  # alles kaputt (с нем. досл. "все сломано". - прим. ред.)
-            if alles_kaputt:
-                return alles_kaputt
-        return ''
+            self.create_person(person)
 
     # делаем пропуски для гостей/посетителей
-    # удивительно, но даже тут могут быть ошибки, текст которых возвращается, если они есть
-    def create_guests(self, start_number: int, total_number: int, ) -> str:
+    def create_guests(self, start_number: int, total_number: int):
+        if start_number <= 0 or total_number <= 0:
+            raise Exception('Ты че, самый умный? А ну живо убрал ненатуральные числа!\n Are you kidding me? Remove non-natural numbers NOW!')
+
         # группа крови - на рукаве
         # мой порядковый номер- на рукаве...
         # (serial_number - порядковый номер. - прим. ред.)
-
-        if start_number <= 0 or total_number <= 0:
-            return 'Ты че, самый умный? А ну живо убрал ненатуральные числа!\n Are you kidding me? Remove non-natural numbers NOW!'
 
         for serial_number in range(start_number, start_number+total_number):
             self.pdf.add_page()  # добавляем новую страничку
@@ -115,13 +106,11 @@ class GenPdf:
             self.set_color_n_size(0, 128, 0, 9)
             self.pdf.text(5, 50, 'ПРИ ВЫХОДЕ ОПУСТИТЕ КАРТУ В ЩЕЛЬ КАРТОПРИЁМНИКА')  # но никакой же щели картоприемника нету на входе в сунец, что за несуразица?
 
-        return ''  # вернули пустоту в знак того, что все ок
-
     # мы писали, мы писали - наши пальчики устали
     # (поэтому вместо тетрадки пишем в pdf)
     def write(self, output_file: str):
         if self.already_written:  # чтобы еще раз напечатать pdf-ку надо заново инициализировать экземпляр
-            return
+            raise Exception('Этот экземпляр класса уже однажды выписал пропуска (дайте ему отдохнуть, он устал). Чтобы еще раз написать pdf, надо переинициализировать его')
         self.pdf.output(output_file)
         self.already_written = True
 
@@ -131,7 +120,7 @@ class GenPdf:
 
 if __name__ == '__main__':
     generator = GenPdf()
-    generator.create_person(Person('Эйнштейн', 'Альберт', Status.student, True, date(2023, 9, 1), 'src/test_photo.jpg'))
+    generator.create_person(Person('Эйнштейн', 'Альберт', date(2023, 9, 1), Status.student, True, 'src/test_photo.jpg'))
     generator.write('aboba.pdf')
     
     # в сотый раз повторяю, что для повторной генерации надо переинициализировать экземпляр класса
