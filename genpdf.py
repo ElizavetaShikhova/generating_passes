6871b826af8e9b5d25058d005b84f0868ef14e6e
meanlessnessener <mr.sansanich05@mail.ru>
2022-06-27 23:57:12 +0500

    comments become more adequate and useful

--- genpdf.py
+++ genpdf.py
@@ -6,24 +6,30 @@ from fpdf import FPDF, set_global
 
 class GenPdf:
     def __init__(self):
-        set_global('FPDF_CACHE_MODE', 1)  # отключение кэширования шрифтов (какую-то странную фичу добавили в этот ваш fpdf, нормальному человеку ничего кэшировать не надо)
-        self.pdf = FPDF(unit='mm', format=(85.6, 53.98))  # создаем главный класс для работы с pdf, а также говорим, что будем использовать милиметры, и задаем размер для одной страницы
-        self.pdf.add_font(family='PFM', style='normal', fname='src/pf.ttf', uni=True)  # добавляем
-        self.pdf.add_font(family='PFM', style='bold', fname='src/pfm.ttf', uni=True)   # шрифты
-        self.already_written = False
+        # отключение кэширования шрифтов (какую-то странную фичу добавили в этот ваш fpdf, нормальному человеку ничего кэшировать не надо)
+        set_global('FPDF_CACHE_MODE', 1)
+        # создаем главный класс для работы с pdf, а также говорим, что будем использовать милиметры, и задаем размер для одной страницы
+        self.pdf = FPDF(unit='mm', format=(85.6, 53.98))
+        # добавляем шрифты
+        self.pdf.add_font(family='PFM', style='normal', fname='src/pf.ttf', uni=True)
+        self.pdf.add_font(family='PFM', style='bold', fname='src/pfm.ttf', uni=True)
         # к сожалению этот прекрасый представитель программной фауны может "родить" pdf только один раз в жизни
         # чтобы еще раз напечатать pdf-ку надо заново инициализировать экземпляр
+        self.already_written = False
 
-    # метод для более удобной смены цвета текста и его размера, аргументы и названия методов self.pdf интуитивно понятны
-    def set_color_n_size(self, r, g, b, size):
+    def set_color_n_size(self, r: int, g: int, b: int, size: int):
+        """
+        метод для более удобной смены цвета текста и его размера
+        """
         self.pdf.set_text_color(r, g, b)
         self.pdf.set_font_size(size)
 
-    # данный метод генерирования шаблона пригождается для создания пропуска одному человеку и гостю/посетителю
     def __create_template(self):
+        """
+        данный метод рисует шаблон, одинаковый для всех типов пропусков
+        """
         self.pdf.image('src/u.png', x=5, y=3, w=9.4, h=9, type='PNG')  # рисуем букву У
 
-        # дальше все понятно, гы)
         self.pdf.set_font('PFM', 'bold')
 
         self.set_color_n_size(0, 0, 0, 11)
@@ -32,16 +38,17 @@ class GenPdf:
         self.set_color_n_size(0, 128, 0, 8.1)
         self.pdf.text(20, 10, 'СПЕЦИАЛИЗИРОВАННЫЙ УЧЕБНО-НАУЧНЫЙ ЦЕНТР')
 
-    # метод для создания одной странички pdf одному человеку
     def create_person(self, person: Person):
-        self.pdf.add_page()  # добавляем страничку
+        """
+        метод для создания одной странички pdf одному человеку
+        """
+        self.pdf.add_page()
+        self.__create_template()
 
-        self.__create_template()  # рисуем шаблончик
-
-        # пишем фамилию и имя
+        # в оригинале отступ между строчками составляет 6мм, что сохранено в данной реализации
         self.set_color_n_size(0, 0, 0, 16)
         self.pdf.text(32, 20, person.surname)
-        self.pdf.text(32, 26, person.name)  # в оригинале отступ между строчками составляет 6мм, что сохранено в данной реализации
+        self.pdf.text(32, 26, person.name)
 
         # пишем статус человечка
         # в оригинале было так, что если статус == обучающийся, то это пишется на одной строке
@@ -51,66 +58,70 @@ class GenPdf:
         if person.status == Status.student:  # простому работяге-ученику не нужны много строчек - хватит и одной
             self.pdf.text(32, 34, person.status.value)
         else:
-            status_array = person.status.value.split(' ', 1)  # разделяем статус на первое слово и оставшиеся слова
-            self.pdf.text(32, 34, status_array[0])  # пихаем
-            self.pdf.text(32, 38, status_array[1])  # статус   (в оригинале отступ между строчками составляет 4мм, что сохранено в данной реализации)
+            # разделяем статус на первое слово и оставшиеся слова
+            # в оригинале отступ между строчками составляет 4мм, что сохранено в данной реализации
+            status_array = person.status.value.split(' ', 1)
+            self.pdf.text(32, 34, status_array[0])
+            self.pdf.text(32, 38, status_array[1])
 
         # если работяга-ученик живет не в Екб
         if person.dorm:
-            self.pdf.image('src/hostel.png', 70, 30, 9.3, 8, type='PNG')  # то награждаем его за стремление к учебе и рисуем ему красивый красный домик, обозначающий общагу
-            # - любите общагу!, - говорит один
-            # - мать вашу!, - отвечают остальные
-            # (общажный мем)
+            # то награждаем его за стремление к учебе и рисуем ему красивый красный домик, обозначающий общагу
+            self.pdf.image('src/hostel.png', 70, 30, 9.3, 8, type='PNG')
 
-        self.pdf.set_font('PFM', 'normal')  # переключаемся на буквально "нормальный" шрифт
+        self.pdf.set_font('PFM', 'normal')
         self.set_color_n_size(0, 0, 0, 10)
-        self.pdf.text(32, 45, f'Действительно по {person.expired.strftime("%d.%m.%Y")}')  # и пишем грустные слова о том, когда придется покидать наш любимый СУНЦ УрФУ
+        # пишем грустные слова о том, когда придется покидать наш любимый СУНЦ УрФУ
+        self.pdf.text(32, 45, f'Действительно по {person.expired.strftime("%d.%m.%Y")}')
 
         try:
-            self.pdf.image(person.photo, 5, 14, 24, 32, type='JPG')  # если все круто, то маленький художник, который сидит в FPDF.pdf.image, рисует главного героя этого пропуска
-        except RuntimeError:  # но всегда что-то может пойти не так, например, фото недоступно
-            raise Exception(f'Фотография для {person.name + " " + person.surname} не найдена')
+            self.pdf.image(person.photo, 5, 14, 24, 32, type='JPG')
+        except RuntimeError:
+            raise Exception(f'Фотография для {person.surname + " " + person.name} не найдена')
 
-        self.pdf.set_font('PFM', 'bold')  # пока вместо начального шрифта властвовал "нормальный", тот успел навестить бабушку в деревне и от этого потолстеть (ох уж эти бабушки, всегда тебя накормят, даже если ты только что сотню пирожков съел)
+        self.pdf.set_font('PFM', 'bold')
         self.set_color_n_size(0, 128, 0, 7)
-        event_name = 'МЕРОПРИЯТИЯ' if person.status == Status.participant else 'ОБУЧЕНИЯ'  # название "мероприятия" в родительном падеже идет в переменную
+        # название "мероприятия" в родительном падеже идет в переменную
+        event_name = 'МЕРОПРИЯТИЯ' if person.status == Status.participant else 'ОБУЧЕНИЯ'
         self.pdf.text(20, 51, f'ПО ОКОНЧАНИИ {event_name} ПОДЛЕЖИТ ВОЗВРАТУ')
 
-    # метод для генерации пропусков для всех, кто есть в списке
     def create_group(self, people: List[Person]):
+        """
+        метод для генерации пропусков всем, кто есть в списке
+        """
         for person in people:
             self.create_person(person)
 
-    # делаем пропуски для гостей/посетителей
     def create_guests(self, start_number: int, total_number: int):
+        """
+        делаем пропуски для гостей/посетителей
+        """
         if start_number <= 0 or total_number <= 0:
-            raise Exception('Ты че, самый умный? А ну живо убрал ненатуральные числа!\n Are you kidding me? Remove non-natural numbers NOW!')
-
-        # группа крови - на рукаве
-        # мой порядковый номер- на рукаве...
-        # (serial_number - порядковый номер. - прим. ред.)
+            raise Exception('Ты че, самый умный? А ну живо убрал ненатуральные числа!')
 
         for serial_number in range(start_number, start_number+total_number):
-            self.pdf.add_page()  # добавляем новую страничку
-            self.__create_template()  # рисуем шаблончик
+            self.pdf.add_page()
+            self.__create_template()
 
             self.pdf.set_font('PFM', 'bold')
             self.set_color_n_size(0, 0, 0, 20)
-            self.pdf.text(25, 25, 'ПОСЕТИТЕЛЬ')  # нет блин, гость
+            self.pdf.text(25, 25, 'ПОСЕТИТЕЛЬ')
 
             self.pdf.set_font('PFM', 'normal')
             self.set_color_n_size(0, 0, 0, 16)
-            self.pdf.text(35, 35, f'№ {str(serial_number).rjust(4, "0")}')  # с помощью магии делаем так, чтобы номер всегда был 4-значным
+            # с помощью магии делаем так, чтобы номер всегда был 4-значным
+            self.pdf.text(35, 35, f'№ {str(serial_number).rjust(4, "0")}')
 
             self.pdf.set_font('PFM', 'bold')
             self.set_color_n_size(0, 128, 0, 9)
-            self.pdf.text(5, 50, 'ПРИ ВЫХОДЕ ОПУСТИТЕ КАРТУ В ЩЕЛЬ КАРТОПРИЁМНИКА')  # но никакой же щели картоприемника нету на входе в сунец, что за несуразица?
+            # но никакой же щели картоприемника нету на входе в сунец, что за несуразица?
+            self.pdf.text(5, 50, 'ПРИ ВЫХОДЕ ОПУСТИТЕ КАРТУ В ЩЕЛЬ КАРТОПРИЁМНИКА')
 
-    # мы писали, мы писали - наши пальчики устали
-    # (поэтому вместо тетрадки пишем в pdf)
     def write(self, output_file: str):
-        if self.already_written:  # чтобы еще раз напечатать pdf-ку надо заново инициализировать экземпляр
-            raise Exception('Этот экземпляр класса уже однажды выписал пропуска (дайте ему отдохнуть, он устал). Чтобы еще раз написать pdf, надо переинициализировать его')
+        # чтобы еще раз напечатать pdf-ку надо заново инициализировать экземпляр
+        if self.already_written:
+            raise Exception('Этот экземпляр класса уже однажды выписал пропуска (дайте ему отдохнуть, он устал).'
+                            'Чтобы еще раз написать pdf, надо переинициализировать его')
         self.pdf.output(output_file)
         self.already_written = True
 
