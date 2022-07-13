6235336d2dbdcc190c43fd63aeed04f9d59179e4
meanlessnessener <mr.sansanich05@mail.ru>
2022-07-13 17:33:35 +0500

    add icons

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+import ctypes
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID('Liza_plus_Sasha_inc')
+
 import sys
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QSize
 from front import MainWindow
 
 app = QApplication(sys.argv)
+icon = QIcon()
+icon.addFile('src/icons/ico_halfsize.png', QSize(25, 19))
+icon.addFile('src/icons/ico_x1.png', QSize(48, 36))
+icon.addFile('src/icons/ico_x2.png', QSize(96, 72))
+icon.addFile('src/icons/ico_x3.png', QSize(144, 108))
+icon.addFile('src/icons/ico_x4.png', QSize(192, 144))
+icon.addFile('src/icons/ico_512h.png', QSize(683, 512))
+icon.addFile('src/icons/ico_512w.png', QSize(512, 384))
+app.setWindowIcon(icon)
 ex = MainWindow()
 ex.setFixedSize(972, 600)
 ex.show()
